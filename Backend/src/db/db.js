c9166cf7d64bb0e289457861dd5a57f400191cc4
Adiii1903 <jadhavaditya8378@gmail.com>
2026-01-02 15:00:40 +0530

    Initial commit

---
+++ Backend/src/db/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+
+function connectDB(){
+    mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("mongodb connected");
+    })
+    .catch((err) => {
+        console.log("mongodb not connected some error", err); 
+    })
+}
+
+module.exports = connectDB;
