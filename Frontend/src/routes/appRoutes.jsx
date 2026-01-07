8f28158037656064d9a1357b389bfaff4182a6f2
Adiii1903 <jadhavaditya8378@gmail.com>
2026-01-07 21:23:54 +0530

    frontend changes and some error solving

--- Frontend/src/routes/appRoutes.jsx
+++ Frontend/src/routes/appRoutes.jsx
@@ -1,24 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import RegisterUser from "./RegisterUser.jsx";
-import LoginUser from "./LoginUser.jsx";
-import RegisterPartner from "./RegisterPartner.jsx";
-import LoginPartner from "./LoginPartner.jsx";
-import "../styles/auth.css";
+import UserRegister from "../pages/auth/UserRegister.jsx";
+import UserLogin from "../pages/auth/UserLogin.jsx";
+import FoodPartnerRegister from "../pages/auth/FoodPartnerRegister.jsx";
+import FoodPartnerLogin from "../pages/auth/FoodPartnerLogin.jsx";
+import "../styles/auth-shared.css";
 
 const AuthChoice = () => {
     return (
-        <div className="auth-page">
-            <div className="auth-card" style={{ textAlign: "center" }}>
-                <div className="brand">Get started</div>
-                <div className="hint">Choose how you'd like to register</div>
-
-                <div className="form" style={{ alignItems: "stretch", gap: "12px" }}>
-                    <Link to="/user/register" className="btn" style={{ display: "block", textDecoration: "none" }}>Register as User</Link>
-                    <Link to="/food-partner/register" className="btn" style={{ display: "block", textDecoration: "none" }}>Register as Food Partner</Link>
-                </div>
-            </div>
-        </div>
+       <UserRegister/>
     );
 };
 
@@ -27,10 +17,10 @@ const AppRoutes = () => {
         <Router>
             <Routes>
                 <Route path="/" element={<AuthChoice />} />
-                <Route path="/user/register" element={<RegisterUser />} />
-                <Route path="/user/login" element={<LoginUser />} />
-                <Route path="/food-partner/register" element={<RegisterPartner />} />
-                <Route path="/food-partner/login" element={<LoginPartner />} />
+                <Route path="/user/register" element={<UserRegister />} />
+                <Route path="/user/login" element={<UserLogin />} />
+                <Route path="/food-partner/register" element={<FoodPartnerRegister />} />
+                <Route path="/food-partner/login" element={<FoodPartnerLogin />} />
             </Routes>
         </Router>
     );
