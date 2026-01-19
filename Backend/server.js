67db7d6fba343ca355079845d6a678d38f52686d
Adiii1903 <jadhavaditya8378@gmail.com>
2026-01-19 13:54:37 +0530

    fix vercel serverless entry

--- Backend/server.js
+++ Backend/server.js
@@ -9,20 +9,31 @@
 //     console.log("Server is running on port 3000");
 // })
 
-require("dotenv").config(); // MUST be at top
 
+
+// require("dotenv").config(); // MUST be at top
+
+// const app = require("./src/app");
+// const connectDB = require("./src/db/db");
+
+// const PORT = process.env.PORT || 3000;
+
+// connectDB()
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log(` Server running on port ${PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error("DB connection failed:", err.message);
+//   });
+
+
+
+require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/db/db");
 
-const PORT = process.env.PORT || 3000;
+connectDB();
 
-connectDB()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(` Server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("DB connection failed:", err.message);
-  });
-
+module.exports = app; // ✅ DO NOT use app.listen
