253061248991c028d8ad2373b1ef13baf62363f3
Adiii1903 <jadhavaditya8378@gmail.com>
2026-01-22 23:12:56 +0530

    change index.js

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,17 +1,36 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "FoodView backend is running 🚀"
-  });
-});
+// app.get("/", (req, res) => {
+//   res.status(200).json({
+//     success: true,
+//     message: "FoodView backend is running 🚀"
+//   });
+// });
 
-app.use(express.json());
+// app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Food View Backend is running 🚀");
-});
+// app.get("/", (req, res) => {
+//   res.send("Food View Backend is running 🚀");
+// });
 
-module.exports = app;
+// module.exports = app;
+
+
+import { connectDB } from "../db/db.js";
+
+export default async function handler(req, res) {
+  try {
+    await connectDB();
+
+    res.status(200).json({
+      success: true,
+      message: "FoodView backend is running 🚀"
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message
+    });
+  }
+}
