193fb3c511b4c69c619c89a9b167b3406f851a0a
Adiii1903 <jadhavaditya8378@gmail.com>
2026-01-23 00:18:17 +0530

    change app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,3 +1,34 @@
+// const express = require("express");
+// const cookieParser = require("cookie-parser");
+// const cors = require("cors");
+
+// const authRoutes = require("./routes/auth.routes");
+// const foodRoutes = require("./routes/food.routes");
+// const foodPartnerRoutes = require("./routes/food-partner.routes");
+
+// const app = express();
+
+// app.use(cors({
+//   origin: [
+//     "http://localhost:5173",
+//     "https://zomato-reels.vercel.app"
+//   ],
+//   credentials: true
+// }));
+
+// app.use(cookieParser());
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.send("Backend is running 🚀");
+// });
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/food", foodRoutes);
+// app.use("/api/food-partner", foodPartnerRoutes);
+
+// module.exports = app;
+
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
@@ -9,10 +40,7 @@ const foodPartnerRoutes = require("./routes/food-partner.routes");
 const app = express();
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://zomato-reels.vercel.app"
-  ],
+  origin: true,
   credentials: true
 }));
 
